9eaf7d02d21873a0a2c8077173e0e3db4d27ffd2
Anders Hagström <86352637+Hagstring@users.noreply.github.com>
2023-12-04 19:57:56 +0100

    Add files via upload

--- Software/Matlab/Catflap_Batterymonitor.m
+++ Software/Matlab/Catflap_Batterymonitor.m
@@ -1,72 +1,38 @@
-% clc 
-% x = [0 0.05 0.25 0.5 0.75 1 1.25 1.5 1.62 1.75 2 2.1 2.25 2.33 2.44 2.5 2.53];
-% y = [1.59 1.5 1.4 1.34 1.3 1.28 1.24 1.22 1.2 1.18 1.13 1.1 1.04 1 0.9 0.82 0.7];
-%  
-% % 1.59    0
-% % 1.5     0.05
-% % 1.4     0.25
-% % 1.34    0.5 
-% % 1.3     0.75
-% % 1.28    1
-% % 1.24    1.25
-% % 1.22    1.5
-% % 1.2     1.62
-% % 1.18    1.75
-% % 1.13    2
-% % 1.1     2.1
-% % 1.04    2.25
-% % 1       2.33
-% % 0.9     2.44
-% % 0.82    2.5
-% % 0.7     2.53
-% %xq = 0:pi/16:pi;
-% intpol = interp1(x,y,'spline');
-% plot(x,y,x,intpol)
+clear all
 clc
 clf
-%x = [0 0.05 0.25 0.5 0.75 1 1.25 1.5 1.62 1.75 2 2.1 2.25 2.33 2.44 2.5 2.53];
-%y = [1.59 1.5 1.4 1.34 1.3 1.27 1.24 1.22 1.2 1.18 1.13 1.1 1.04 1 0.9 0.82 0.7];
+x = [0 0.0625 0.125 0.25 0.5 0.75 1 1.25 1.5 1.75 2 2.25 2.375 2.5];
+y = 4*[1.59 1.5 1.43 1.4 1.34 1.3 1.27 1.24 1.22 1.18 1.14 1.04 0.94 0.82];
 
-x = [0 0.25 0.5 0.75 1 1.25 1.5 1.75 2 2.25 2.5];
-y = 4*[1.59 1.4 1.34 1.3 1.27 1.24 1.22 1.18 1.14 1.04 0.82];
+pINV = polyfit(y,x,3);
+y_values = linspace(max(y), min(y), 100); % Generating x values for plotting the polynomial
+x_fit = polyval(pINV, y_values); % Evaluating the polynomial at x_values
 
-xq = 0:0.001:2.53; % Define the query points where you want to interpolate
-
-intpol = interp1(x, y, xq, 'spline'); % Interpolate y values corresponding to xq
-
-p = polyfit(x,y,7);
-x_values = linspace(min(x), max(x), 100); % Generating x values for plotting the polynomial
-y_fit = polyval(p, x_values); % Evaluating the polynomial at x_values
-y_range = linspace(6.36,3.6,100);
-
-x_lin = linspace(0,2.5,11);
-y_lin = 4*linspace(1.59,0.9,11);
-
-BatteryVoltage = y_lin
-
-BatteryPercentage = (BatteryVoltage-3.6)/.0276
-BatteryPercentage2 = (y-3.6)/.0276
+%YNEW = pINV(1)*y_values.^10+pINV(2)*y_values.^9+pINV(3)*y_values.^8+pINV(4)*y_values.^7+pINV(5)*y_values.^6+pINV(6)*y_values.^5+pINV(7)*y_values.^4+pINV(8)*y_values.^3+pINV(9)*y_values.^2+pINV(10)*y_values.^1+pINV(11); %10
+%YNEW = pINV(1)*y_values.^7+pINV(2)*y_values.^6+pINV(3)*y_values.^5+pINV(4)*y_values.^4+pINV(5)*y_values.^3+pINV(6)*y_values.^2+pINV(7)*y_values.^1+pINV(8); %7
+%YNEW = pINV(1)*y_values.^5+pINV(2)*y_values.^4+pINV(3)*y_values.^3+pINV(4)*y_values.^2+pINV(5)*y_values.^1+pINV(6); %5
+%YNEW = pINV(1)*y_values.^4+pINV(2)*y_values.^3+pINV(3)*y_values.^2+pINV(4)*y_values+pINV(5); %4
+%YNEW = pINV(1)*y_values.^3+pINV(2)*y_values.^2+pINV(3)*y_values+pINV(4); %3
+YNEW = -0.6198*y_values.^3+2.183*y_values.^2-2.725*y_values+6.199; %3
+%YNEW = pINV(1)*y_values.^2+pINV(2)*y_values+pINV(3); %3
 
 
+%1/0.01+e^-1.4x
 
-y_per = (y_fit-3.6)/0.0276
+YPlot=100*(((2.5-YNEW)/2.5));
+XPlot=100*(((2.5-x)/2.5));
+BatteryPercentage = (y-3.28)/.0308; % Vmax-Vmin/100=0.0308
 
-x_per = 100*(1-((6.36-y_lin)/2.5))
-
-x_perdata = 100*(((2.5-x)/2.5))
-
-plot(x, y, 'o', x_lin, y_lin,'x',x_values, y_fit);
-legend('Original Data','Lin','Polyfit');
+%Vmax=4*1.59=6.36 Vmin=0.9*4=3.6 0.0276=(Vmax-VMin)/100 	LINEAR.. :(
+figure (1);
+plot(y_values,YPlot,y,XPlot,'o',y,BatteryPercentage);
+%set (gca,'XDir','reverse');
+%legend('Original Data','Polyfit','Lin');
 grid on
 
-figure (2);
-plot(x,BatteryPercentage2,'o' ,x,BatteryPercentage,'x',x_values, y_per)
-legend('Original Data','Linear');
-grid on
+figure(2);
+plot(y,XPlot,'o',y_values,YPlot,y,BatteryPercentage)
 
-figure (3);
-plot(y_lin, x_per, 'o', y, x_perdata, 'x')
-set (gca,'XDir','reverse'); 
-legend('Original Data');
-grid on
 
+figure(3);
+plot(x,y,'o')
