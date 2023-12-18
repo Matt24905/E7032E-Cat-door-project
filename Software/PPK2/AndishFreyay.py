d3672a0c84817eba718276b81d728f47be48da20
Anders Hagström <86352637+Hagstring@users.noreply.github.com>
2023-12-18 11:26:51 +0100

    Add files via upload

---
+++ Software/PPK2/AndishFreyay.py
@@ -0,0 +1,189 @@
+# # rapid_read_gpio_values
+import time
+start = time.time()
+
+
+
+
+def rapid_read_gpio_values(pin_number, duration_us):
+#     gpio_pin = machine.Pin(pin_number, machine.Pin.IN, machine.Pin.PULL_DOWN)
+#     end_time = utime.ticks_add(utime.ticks_us(), duration_us)
+#     measurements = []
+
+#     while utime.ticks_diff(end_time, utime.ticks_us()) > 0:
+#         measurements.append(gpio_pin.value())
+    
+
+    with open("EMBINARY copy.txt", "r") as file:
+    #with open("GoodTestEM410x.pm3", "r") as file:
+        measurements = [int(value) for value in file.read().split()]
+    measurements = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1]
+
+    return measurements
+
+
+
+# classify_and_convert
+def classify_and_convert(data, threshold):
+    bits = []
+    current_value = data[0]
+    count = 0
+
+    for value in data:
+        if value == current_value:
+            count += 1
+        else:
+            if count >= threshold:
+                bits.extend([current_value, current_value])
+            else:
+                bits.append(current_value)
+            count = 1
+            current_value = value
+
+    # Handle the last segment
+    if count >= threshold:
+        bits.extend([current_value, current_value])
+    else:
+        bits.append(current_value)
+
+    return bits
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# process_gpio_signal
+def process_gpio_signal(values):
+    # Find the index where the header pattern starts
+    header_pattern = [1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]  # Converted to a list of integers
+    for i in range(len(values) - 18):
+        if values[i:i+19] == header_pattern:
+            break
+    if i == -1:
+        return "Header pattern not found"
+
+    # Calculate the end index based on the start index and total bits to capture
+    if i + 129 > len(values):
+        return "Header pattern found, but data is too short"
+
+    # Extract the relevant portion of the signal after the header pattern
+    captured_bits = values[i + 1:i + 129]
+
+    return captured_bits
+
+# decode_inverted_manchester
+def decode_inverted_manchester(encoded_signal):
+    decoded_bits = []
+
+    # Loop through the signal, two elements at a time
+    for i in range(0, 127, 2):
+        # Combine the two bits for easier comparison
+        bit_pair = (encoded_signal[i], encoded_signal[i+1])
+
+        # Check for transitions and decode
+        if bit_pair == (1, 0):
+            decoded_bits.append(1)
+        elif bit_pair == (0, 1):
+            decoded_bits.append(0)
+        # Optional: Handle invalid transitions, e.g., (0, 0) or (1, 1)
+        else:
+            # Handle invalid transition
+            break
+
+    # Convert the list of bits into a string
+    decoded_string = ''.join(str(bit) for bit in decoded_bits)
+    return decoded_string
+
+# decode_em4100
+def decode_em4100(array):
+    if len(array) != 64:
+       return "Invalid input: String not correct length."
+
+    if array[:9] != '111111111':
+        return "Invalid input: Header bits are incorrect."
+    
+    if array[-1] != '0':
+        return "Invalid input: Stop bit is incorrect."
+    
+    data_bits_with_parity = array[9:-5]
+    data_bits = ''
+
+    for i in range(0, 50, 5):
+        data = data_bits_with_parity[i:i+4]
+        parity = data_bits_with_parity[i+4]
+        if data.count('1') % 2 != int(parity):
+            return "Invalid input: Parity check failed for group " + data
+        data_bits += data
+    
+    hex_result = format(int(data_bits, 2), '010X')
+    return hex_result
+
+
+# Example usage
+def main():
+
+    # Measure ra256pidly for 65536 microseconds
+    raw_measurements = rapid_read_gpio_values(0, 65536)
+    #print("raw_measurements",raw_measurements)
+    c = time.time() - start
+    print('after raw_measurements:',c)
+    threshold = 16 # Example threshold, adjust based on your data
+    converted_bits = classify_and_convert(raw_measurements, threshold)
+
+    e = time.time() - start
+    print('after classify_and_convert:',e)
+
+
+    #normalized_measurements = '1010101010101100110010101100101101010011010101010011001011001100101011001101010101010101010010101010110101001100101010101010101010101010101011001100101011001011010100110101010100110010110011001010110011010101010101010100101010101101010011001010101010101010'
+    #normalized_measurements = [int(normalized_measurements) for normalized_measurements in normalized_measurements]
+    #print("Measurement", normalized_bits)
+
+    captured_bits = process_gpio_signal(converted_bits)
+    #print("Process_gpio_signal", captured_bits)
+    b = time.time() - start
+    print('after process_gpio_signal:',b)
+
+    # Check if captured_bits is a string message or actual data
+    if isinstance(captured_bits, str):
+        print(captured_bits)  # Print error message
+        # Delay before the next read if the header pattern is not found or data is too short
+        #utime.sleep(1)
+        #continue  # Skip the rest of the loop and try again
+
+
+    # Remove when test
+    #captured_bits = '10101010101010101001010101011010100110010101010101010101010101010101100110010101100101101010011010101010011001011001100101011001'
+    #captured_bits = [int(captured_bits) for captured_bits in captured_bits]
+
+    decoded_signal = decode_inverted_manchester(captured_bits)
+    u = time.time() - start
+    print('after decode_inverted_manchester:',u)
+    #print("Decode_inverted_manchester:", decoded_signal)
+    #decoded_signal = '1111111110000011101000000000000000101000100111011111010010100010'
+    # Should be 0E000A4DE2
+    valid_tag = decode_em4100(decoded_signal)
+    if "Invalid input" not in valid_tag:
+        print("Valid tag found:", valid_tag)
+        #break  # Exit the loop if a valid tag is found
+        #else:
+            #print(valid_tag)  # Print the error message
+            #utime.sleep(1)  # Delay before the next read if the tag is invalid
+# Run the main function
+main()
+
+t = time.time() - start
+print('Time:',t)
+
+
+
+
